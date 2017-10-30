1f1ab2b59fc8cd180a1b1814bb6a0eb250209b32
slim <=>
2017-10-30 08:12:57 +0100

    threads tricks

--- threads.py
+++ threads.py
@@ -1,4 +1,6 @@
 from threading import RLock
+import _thread
+import time
 
 lock = RLock()
 
@@ -9,14 +11,33 @@ def fac_recursive(n):
 	else:
 		return n * fac_recursive(n-1)
 
-if __name__ == '__name__':
-	print fac_recursive(5)
+def print_time(thread_name, delay):
+	count = 0
+	while count < 5:
+		time.sleep(delay)
+		count += 1
+		print("{} : {}".format(thread_name, time.ctime(time.time())) )
 
-"""
-what's the output ?
-1. 120
-2. code blocked to due to the lock in the second recursion
-3. after the lock, it prints 120
-4. Exception raised
 
-"""
+
+
+# if __name__ == '__name__':
+	# print fac_recursive(5)
+
+	"""
+	what's the output ?
+	1. 120
+	2. code blocked to due to the lock in the second recursion
+	3. after the lock, it prints 120
+	4. Exception raised
+
+	"""
+try:
+	_thread.start_new_thread(print_time, ("Thread-1", 2))
+	_thread.start_new_thread(print_time, ("Thread-2", 4))
+except:
+	print("Error: unable to start thread")
+
+while True:
+	pass
+
